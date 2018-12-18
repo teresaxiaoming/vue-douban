466d00ad3641801f23fd3df5a1cd70f74e23637e
teresaXDX <1094930121@qq.com>
2018-12-18 16:32:30 +0800

    vue-douban

---
+++ precache-manifest.9a738912f52446071373feaac20d57b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2894918c2d3fad3f8337",
+    "url": "/js/chunk-vendors.8e475858.js"
+  },
+  {
+    "revision": "01484b403d388cce95c5",
+    "url": "/js/chunk-3b56691c.ac81b208.js"
+  },
+  {
+    "revision": "73a45ac9d26f01fd6fe9",
+    "url": "/js/app.055fcca4.js"
+  },
+  {
+    "revision": "7ebe76c3a1b36d50fae9413fd3667203",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01484b403d388cce95c5",
+    "url": "/css/chunk-3b56691c.2ae72c23.css"
+  },
+  {
+    "revision": "73a45ac9d26f01fd6fe9",
+    "url": "/css/app.0f330ca7.css"
+  }
+];
